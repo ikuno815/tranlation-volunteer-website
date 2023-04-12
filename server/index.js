31f45e2c4a5d4b162cc7b629a57ea6d6458d6180
ikuno815 <ikuno.kanasugi19@gmail.com>
2023-04-12 19:31:07 +0900

    feat: initialize & configure express server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const setupServer = require('./server');
+const PORT = process.env.PORT || 4000;
+const server = setupServer();
+
+(async() => {
+    try {
+        server.listen(PORT, () => {
+            console.log(`app is listening @ http://localhost:${PORT}`);
+        });
+        
+    } catch (err) {
+        console.error(`app failed to start ${err}`)
+    }
+})();
---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+
+function setupServer() {
+    const app = express();
+
+    app.use(express.json());
+
+    app.get('/hello', (req, res) => {
+        res.send('world🌎')
+    });
+
+    return app;
+}
+
+module.exports = setupServer;
