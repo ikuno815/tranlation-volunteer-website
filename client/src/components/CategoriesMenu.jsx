7a411918e0012766a537b75af8ffbadc524b64c0
ikuno815 <ikuno.kanasugi19@gmail.com>
2023-04-18 20:58:36 +0900

    feat: make react router dom components

---
+++ client/src/components/CategoriesMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CategoriesMenu() {
+  return (
+    <div>CategoriesMenu</div>
+  )
+}
+
+export default CategoriesMenu
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { UserAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+export default function Navbar() {
+    const {user, logOut} = UserAuth();
+    const navigate = useNavigate();
+  
+    const handleLogout = async() => {
+      try {
+          await logOut();
+          navigate('/');
+      } catch (err) {
+          console.error(err);
+      }
+    }
+  return (
+    <div>
+        <p>Hi! {user && user.email}</p>
+        <button onClick={() => navigate('/mypage')}>My Page</button>
+        <button onClick={handleLogout}>Logout</button>
+    </div>
+  )
+}
