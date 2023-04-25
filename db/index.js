985e40e3fe09c9664df77002f7aa04c3f5e05bf4
ikuno815 <ikuno.kanasugi19@gmail.com>
2023-04-25 15:15:09 +0900

    fix: modify package.json scripts

--- db/index.js
+++ db/index.js
@@ -1,4 +1,11 @@
-const knex = require('knex');
-const knexConfigs = require("../knexfile");
+// const knex = require('knex');
+// const knexConfigs = require("../knexfile");
 
-module.exports = knex(knexConfigs[process.env.NODE_ENV]);
+// module.exports = knex(knexConfigs[process.env.NODE_ENV]);
+
+
+const knex = require("knex");
+const knexConfig = require("../knexfile")[process.env.NODE_ENV];
+console.log(process.env.NODE_ENV);
+
+module.exports = knex(knexConfig);
