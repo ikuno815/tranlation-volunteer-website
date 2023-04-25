998564a3e05d55528be0507a59ec72f4f684417f
ikuno815 <ikuno.kanasugi19@gmail.com>
2023-04-25 22:15:11 +0900

    fix: add cors

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors')
 const usersController = require('./users/users.controller');
 const translationRequestController = require('./translation-request/translation-request.controller');
 const categoriesController = require('./categories/categories.controllers');
@@ -9,6 +10,7 @@ const translationSuggestionController = require('./translation-suggestion/transl
 function setupServer() {
     const app = express();
 
+    app.use(cors());
     app.use(express.static(path.resolve(__dirname, '../client/build')));
     app.use(express.json());
 
