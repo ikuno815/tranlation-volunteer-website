d3a03047a72fef00f9926b481521eda80dc077db
ikuno815 <ikuno.kanasugi19@gmail.com>
2023-04-25 22:54:14 +0900

    fix: change + into , of path

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const translationSuggestionController = require('./translation-suggestion/transl
 function setupServer() {
     const app = express();
 
-    app.use(express.static(path.resolve(__dirname + '../client/build')));
+    app.use(express.static(path.resolve(__dirname, '../client/build')));
     app.use(express.json());
 
     app.use('/api/user', usersController);
